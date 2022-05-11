388a05b5194d22837f538636824b4db53a6b4b1b
Joshua Postel <jpostel@umich.edu>
2022-05-11 13:29:56 -0400

    snapshot before transitioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import Button from "./Button";
 import * as TextCollection from "./TextCollection";
-
-class OldApp extends React.Component {
-  render() {
-    return (
-      <div>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <button>Foobar</button>
-          <Button>BarFoo</Button>
-        </header>
-      </div>
-    );
-  }
-}
 
 interface GreetingProps {
   name: string,
