a3a46dd9a686c49fcd8bec5aadbf11561cb2971a
Joshua Postel <jpostel@umich.edu>
2022-05-11 22:07:57 -0400

    polish

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ class App extends React.Component {
         <img className="Screen-shot" src={deepGlissCapture} alt="Screen Shot"/>
         <h1 className="Section-header">Download</h1>
 	<div className="Center-text">
-	  <text>This is an <b>alpha</b> release - you may incounter bugs and there are <a href="https://github.com/JoshuaPostel/DeepGliss/issues">known issues</a></text>
+	  <text>This is an <b>alpha</b> release - there are <a href="https://github.com/JoshuaPostel/DeepGliss/issues">known issues</a> and you may encounter bugs</text>
 	</div>
         <div className="Download-buttons-row">
           <Button.Linux />
@@ -32,24 +32,23 @@ class App extends React.Component {
 	  <SoundCloud.EmbeddedPlayer trackNumber={244308292} hexColor="000000" />
         </div>
         <h1 className="Section-header">Setup</h1>
-	<ul className="Ul">
-	  <li>{'MIDI input -> DeepGliss -> Synthesizer'}</li>
+	<div className="Center-text">
+	  Requires a synthesizer with <a href="https://en.wikipedia.org/wiki/MIDI#MIDI_Polyphonic_Expression">MIDI Polyphonic Expression</a> support such as <a href="https://vital.audio/">Vital</a> or <a href="https://www.ableton.com/en/packs/operator/">Operator</a>
+	</div>
+	<ol className="Ul">
+	  <li>Unzip the DeepGliss download and move its contents to your VST2 plugin directory</li>
+	  <li>In your DAW, route MIDI input into DeepGliss and the MIDI output of DeepGliss into your synthesizer</li>
 	  <li>Set DeepGliss's pitchbend range (under settings) to match the synthesizer's pitchbend range</li>
-	  <li>Requires a synthesizer with <a href="https://en.wikipedia.org/wiki/MIDI#MIDI_Polyphonic_Expression">MIDI Polyphonic Expression</a> support such as:</li>
-	  <ul>
-	    <li><a href="https://vital.audio/">Vital</a></li>
-	    <li><a href="https://www.ableton.com/en/packs/operator/">Operator</a></li>
-	  </ul>
-	</ul>
+	</ol>
         <h1 className="Section-header">Feedback</h1>
-	<ul className="Ul">
-	  <li>Feedback via <a href="https://github.com/JoshuaPostel/DeepGliss/issues">Github</a> is greatly appreciated</li>
-	</ul>
+	<div className="Center-text">
+	  Bug reports, feature requests, presets, themes, and general suggestions are greatly appreciated via <a href="https://github.com/JoshuaPostel/DeepGliss/issues">GitHub</a>
+	</div>
         <h1 className="Section-header">About</h1>
-	<ul className="Ul">
-	  <li>The name DeepGliss comes from <a href="https://en.wikipedia.org/wiki/Deep_Note">Deep Note</a> + <a href="https://en.wikipedia.org/wiki/Glissando">Glissando</a></li>
-	</ul>
-      </div>
+	<div className="Center-text">
+	  The name DeepGliss comes from <a href="https://en.wikipedia.org/wiki/Deep_Note">Deep Note</a> + <a href="https://en.wikipedia.org/wiki/Glissando">Glissando</a>
+        </div>
+    </div>
     );
   }
 }
