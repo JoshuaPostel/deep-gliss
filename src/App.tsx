6f4aeee159c3421eb4e3bd26b371d17dcda951e5
Joshua Postel <jpostel@umich.edu>
2022-05-13 10:49:26 -0400

    more polish

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,16 @@ class App extends React.Component {
         <img className="Screen-shot" src={deepGlissCapture} alt="Screen Shot"/>
         <h1 className="Section-header">Download</h1>
 	<div className="Center-text">
-	  <text>This is an <b>alpha</b> release - there are <a href="https://github.com/JoshuaPostel/DeepGliss/issues">known issues</a> and you may encounter bugs</text>
+	  <text>This is an <b>alpha</b> release - there are <a className="Link" href="https://github.com/JoshuaPostel/DeepGliss/issues">known issues</a> and you may encounter bugs</text>
 	</div>
         <div className="Download-buttons-row">
           <Button.Linux />
           <Button.Windows />
           <Button.Mac />
         </div>
+	<div className="Center-text">
+	  <text>Currently MacOS must be built from <a className="Link" href="https://github.com/JoshuaPostel/DeepGliss/">source</a></text>
+	</div>
         <h1 className="Section-header">Audio Samples</h1>
 
         <div className="SoundCloud-player-row">
@@ -32,20 +35,20 @@ class App extends React.Component {
         </div>
         <h1 className="Section-header">Setup</h1>
 	<div className="Center-text">
-	  Requires a synthesizer with <a href="https://en.wikipedia.org/wiki/MIDI#MIDI_Polyphonic_Expression">MIDI Polyphonic Expression</a> support such as <a href="https://vital.audio/">Vital</a> or <a href="https://www.ableton.com/en/packs/operator/">Operator</a>
+	  Requires an instrument with <a className="Link" href="https://en.wikipedia.org/wiki/MIDI#MIDI_Polyphonic_Expression">MIDI Polyphonic Expression</a> support such as <a className="Link" href="https://vital.audio/">Vital</a>, <a className="Link" href="https://www.ableton.com/en/packs/operator/">Operator</a>, or <a className="Link" href="https://www.ableton.com/en/packs/string-quartet/">Spitfire String Quartet</a>
 	</div>
 	<ol className="Ul">
 	  <li>Move the DeepGliss download to your VST2 plugin directory</li>
-	  <li>In your DAW, route MIDI input into DeepGliss and the MIDI output of DeepGliss into your synthesizer</li>
-	  <li>Set DeepGliss's pitchbend range (under settings) to match the synthesizer's pitchbend range</li>
+	  <li>In your DAW, route MIDI input into DeepGliss and the MIDI output of DeepGliss into your instrument</li>
+	  <li>Set DeepGliss's pitchbend range (under settings) to match the instrument's pitchbend range</li>
 	</ol>
         <h1 className="Section-header">Feedback</h1>
 	<div className="Center-text">
-	  Bug reports, feature requests, presets, sounds, and general suggestions are greatly appreciated via <a href="https://github.com/JoshuaPostel/DeepGliss/issues">GitHub</a>
+	  Bug reports, feature requests, presets, sounds, and general suggestions are greatly appreciated via <a className="Link" href="https://github.com/JoshuaPostel/DeepGliss/issues">GitHub</a>
 	</div>
         <h1 className="Section-header">About</h1>
 	<div className="Center-text">
-	  The name DeepGliss comes from <a href="https://en.wikipedia.org/wiki/Deep_Note">Deep Note</a> + <a href="https://en.wikipedia.org/wiki/Glissando">Glissando</a>
+	  The name DeepGliss comes from <a className="Link" href="https://en.wikipedia.org/wiki/Deep_Note">Deep Note</a> + <a className="Link" href="https://en.wikipedia.org/wiki/Glissando">Glissando</a>
         </div>
     </div>
     );
